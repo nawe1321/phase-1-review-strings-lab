8455cd5c454176cb889bb9b5ed58784e8521d151
Nathan Wendlowsky <nathan.wendlowsky@flatironschool.com>
2023-06-30 10:23:13 -0400

    Set variables based on test, changed shortGreeting to slice

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Jeff';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
